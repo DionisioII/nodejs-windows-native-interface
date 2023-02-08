06db897da67e7040d1ebbe0218b8c39f08649af1
giorgio aurispa <jaws24@hotmail.it>
2023-02-08 10:00:44 +0100

    delete cout string

--- demo.js
+++ demo.js
@@ -1,9 +1,4 @@
-//const addon = require( 'D:\\Librerie\\node_libraries\\projects\\emoj_proctor_win_addon\\build\\Release\\proctor_controls' );
-//console.log( addon.testFunction( 'World' ) );
 
-
-
-//const JSVolumeControl  = require('D:\\Librerie\\node_libraries\\projects\\emoj_proctor_win_addon\\build\\Release\\proctor_controls')
 const JSVolumeControl  = require('.\\build\\Release\\proctor_controls')
 console.log(JSVolumeControl)
 
